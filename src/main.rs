7ca3739a2ea386eaf9615bf642aa1c1d7266cdd1
newcomb-luke <newcomb.luke@protonmail.com>
2020-10-24 21:10:33 -0400

    Added files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, process};
+
+use kos-kdump::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searcing for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+
 }
