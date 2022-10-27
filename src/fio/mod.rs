3bb90ab94a49bd56bf2a9dd5e84f853c92881be2
newcomb-luke <newcomb.luke@gmail.com>
2022-10-27 00:11:17 -0400

    Updated to kerbalobjects 4.0.1

--- src/fio/mod.rs
+++ src/fio/mod.rs
@@ -5,27 +5,27 @@ use flate2::read::GzDecoder;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum FileType {
-    KSM,
-    KO,
-    UNKNOWN,
+    KerbalMachineCode,
+    KerbalObject,
+    Unknown,
 }
 
 /// Determines the type of a file using the raw bytes
-pub fn determine_file_type(contents: &Vec<u8>) -> Result<FileType, Box<dyn Error>> {
+pub fn determine_file_type(contents: &[u8]) -> Result<FileType, Box<dyn Error>> {
     if is_gzip(contents) {
-        let mut decoder = GzDecoder::new(&contents[..]);
+        let mut decoder = GzDecoder::new(contents);
         let mut decompressed = [0, 0, 0, 0];
 
         decoder.read_exact(&mut decompressed)?;
 
         if is_ksm(&decompressed) {
-            return Ok(FileType::KSM);
+            return Ok(FileType::KerbalMachineCode);
         }
     } else if is_ko(contents) {
-        return Ok(FileType::KO);
+        return Ok(FileType::KerbalObject);
     }
 
-    Ok(FileType::UNKNOWN)
+    Ok(FileType::Unknown)
 }
 
 /// Checks if the file is in proper GZIP format
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-use clap::ArgMatches;
-use kerbalobjects::kofile::KOFile;
-use kerbalobjects::{ksmfile::KSMFile, FromBytes};
+use clap::Parser;
+use kerbalobjects::ko::KOFile;
+use kerbalobjects::ksm::KSMFile;
+use kerbalobjects::BufferIterator;
 use std::io::Write;
+use std::path::PathBuf;
 use std::{error::Error, fs};
 use termcolor::{Color, ColorSpec, StandardStream};
 
@@ -14,7 +16,7 @@ use output::KSMFileDebug;
 
 pub static NO_COLOR: Color = Color::Rgb(255, 255, 255);
 
-pub static VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub static VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub static ORANGE_COLOR: Color = Color::Rgb(201, 155, 87);
 pub static PURPLE_COLOR: Color = Color::Rgb(133, 80, 179);
@@ -30,89 +32,163 @@ pub fn run(config: &CLIConfig) -> Result<(), Box<dyn Error>> {
 
     writeln!(stream, "kDump version {}", VERSION)?;
 
-    let filename = config.file_path.to_string();
-    let raw_contents = fs::read(filename)?;
-    let mut raw_contents_iter = raw_contents.iter().peekable();
+    let raw_contents = fs::read(&config.file_path)?;
+    let mut raw_contents_iter = BufferIterator::new(&raw_contents);
 
     let file_type = determine_file_type(&raw_contents)?;
 
     match file_type {
-        // If this is a compiled kerbal machine code file
-        FileType::KSM => {
-            let ksm = KSMFile::from_bytes(&mut raw_contents_iter, false)?;
-
+        FileType::KerbalMachineCode => {
+            let ksm = KSMFile::parse(&mut raw_contents_iter)?;
             let ksm_debug = KSMFileDebug::new(ksm);
 
-            ksm_debug.dump(&mut stream, &config)?;
+            ksm_debug.dump(&mut stream, config)?;
 
             Ok(())
         }
-        // If it is a kerbal object file
-        FileType::KO => {
-            let kofile = KOFile::from_bytes(&mut raw_contents_iter, false)?;
-
+        FileType::KerbalObject => {
+            let kofile = KOFile::parse(&mut raw_contents_iter)?;
             let ko_debug = KOFileDebug::new(kofile);
 
-            ko_debug.dump(&mut stream, &config)?;
+            ko_debug.dump(&mut stream, config)?;
 
             Ok(())
         }
         // If we have no idea what the heck the file is
-        FileType::UNKNOWN => {
-            return Err("File type not recognized.".into());
-        }
+        FileType::Unknown => Err("File type not recognized.".into()),
     }
 }
 
 /// This structure controls all the settings that make this program perform differently
 /// These represent command line arguments read in by clap
+#[derive(Debug, Parser)]
+#[command(name = "kDump Utility", author, version, about, long_about = None)]
 pub struct CLIConfig {
-    pub file_path: String,
+    /// The input file path, which is required
+    #[arg(value_name = "FILE", help = "Sets the input file to use")]
+    pub file_path: PathBuf,
+    /// Whether we should disassemble the file's code sections
+    /// Conflicts with disassemble_symbol and full-contents
+    #[arg(
+        short = 'D',
+        long,
+        help = "Disassembles the contents of the entire object file",
+        conflicts_with("disassemble_symbol"),
+        conflicts_with("full_contents")
+    )]
     pub disassemble: bool,
-    pub disassemble_symbol: bool,
-    pub disassemble_symbol_value: String,
+    /// Whether we should disassemble the file's code sections, starting at a particular symbol
+    /// Conflicts with disassemble and full-contents
+    #[arg(
+        short = 'd',
+        long = "disassemble-symbol",
+        help = "Disassembles at the symbol provided until the end of the section",
+        require_equals = true,
+        value_name = "SYMBOL",
+        conflicts_with("disassemble"),
+        conflicts_with("full_contents")
+    )]
+    pub disassemble_symbol: Option<String>,
+    /// Whether we should dump the file headers
+    /// KO only
+    #[arg(
+        short = 'f',
+        long = "file-headers",
+        help = "Displays summary information of the KO file header"
+    )]
     pub file_headers: bool,
+    /// Whether we should dump the argument section contents
+    /// KSM only
+    #[arg(
+        short = 'a',
+        long = "argument-section",
+        help = "Displays the contents of the argument section of a KSM file"
+    )]
     pub argument_section: bool,
+    /// Whether we should display line numbers in disassembled code
+    /// KSM only
+    #[arg(
+        short = 'l',
+        long = "line-numbers",
+        help = "Displays the line numbers of disassembled instructions when disassembling"
+    )]
     pub line_numbers: bool,
+    /// Whether we should dump the section header table
+    /// KO only
+    #[arg(
+        long = "section-headers",
+        help = "Displays the section header table of a KO file"
+    )]
     pub section_headers: bool,
+    /// Whether we should dump the data section of the file
+    /// KO only
+    #[arg(long = "data", help = "Displays each data section of a KO file")]
     pub data: bool,
+    /// Whether we should display the contents of every section in the object file
+    #[arg(
+        short = 's',
+        long = "full-contents",
+        help = "Displays the contents of every section in the object file"
+    )]
     pub full_contents: bool,
+    /// Whether we should dump the string tables of the file
+    /// KO only
+    #[arg(
+        short = 'S',
+        long = "stabs",
+        help = "Displays each string table of a KO file"
+    )]
     pub stabs: bool,
+    /// Whether we should dump the symbol tables of the file
+    /// KO only
+    #[arg(
+        short = 't',
+        long = "syms",
+        help = "Displays each symbol table of a KO file"
+    )]
     pub syms: bool,
+    /// Whether we should dump the relocation data section of the file
+    /// KO only
+    #[arg(
+        short = 'r',
+        long = "reloc",
+        help = "Displays each relocation data table of a KO file"
+    )]
     pub reloc: bool,
+    /// Whether we should display all of the section headers of the file
+    /// KO only
+    #[arg(
+        short = 'x',
+        long = "all-headers",
+        help = "Displays all available KO file header information including the symbol table"
+    )]
     pub all_headers: bool,
+    /// Displays all available meta info of the object file including compiler comments and version information
+    #[arg(
+        short = 'i',
+        long = "info",
+        help = "Displays all available meta info of the object file including compiler comments and version information"
+    )]
     pub info: bool,
+    /// Whether we should attempt to demangle symbol names
+    #[arg(
+        short = 'C',
+        long = "demangle",
+        help = "Tries to demangle disassembled function and variable names"
+    )]
     pub demangle: bool,
+    /// A flag for if we should NOT display raw instruction bytes in the disassembly
+    /// KSM only
+    #[arg(
+        long = "show-no-raw-instr",
+        help = "When disassembling, disables showing the raw bytes that make up each instruction"
+    )]
     pub show_no_raw_instr: bool,
+    /// A flag for if we should NOT display instruction labels in the disassembly
+    /// KSM only
+    #[arg(
+        long = "show-no-labels",
+        help = "When disassembling, disables showing the label of each instruction"
+    )]
     pub show_no_labels: bool,
-}
-
-impl CLIConfig {
-    /// Creates a new CLIConfig using the matches output of clap
-    pub fn new(matches: ArgMatches) -> CLIConfig {
-        CLIConfig {
-            file_path: String::from(matches.value_of("INPUT").unwrap()),
-            disassemble: matches.is_present("disassemble"),
-            disassemble_symbol: matches.is_present("disassemble_symbol"),
-            disassemble_symbol_value: if matches.is_present("disassemble_symbol") {
-                String::from(matches.value_of("disassemble_symbol").unwrap())
-            } else {
-                String::new()
-            },
-            file_headers: matches.is_present("file_headers"),
-            argument_section: matches.is_present("argument_section"),
-            line_numbers: matches.is_present("line_numbers"),
-            section_headers: matches.is_present("section_headers"),
-            data: matches.is_present("data"),
-            full_contents: matches.is_present("full_contents"),
-            stabs: matches.is_present("stabs"),
-            syms: matches.is_present("syms"),
-            reloc: matches.is_present("reloc"),
-            all_headers: matches.is_present("all_headers"),
-            info: matches.is_present("info"),
-            demangle: matches.is_present("demangle"),
-            show_no_raw_instr: matches.is_present("show_no_raw_instr"),
-            show_no_labels: matches.is_present("show_no_labels"),
-        }
-    }
 }
